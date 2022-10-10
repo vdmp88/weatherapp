83ce596340c74dd4cf2372209f92272e1860efdc
Vadim Pakharuk <vadim.pakharuk@nixsolutions.com>
2022-10-10 18:12:04 +0200

    feat: excuse me for SH1T

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { Box, CircularProgress } from '@mui/material';
 import { HomePage } from './components/pages/HomePage/HomePage';
 import { CityPage } from './components/pages/CityPage/CityPage';
 import { NoMatchPage } from './components/pages/NoMatchPage/NoMatchPage';
 import { useGetCitiesQuery } from './services/CitiesService';
-import { useAppSelector } from './hooks/redux';
+import { useAppSelector, useAppDispatch } from './hooks/redux';
+import { setOffset } from './store/reducers/CitiesPagination';
 import './main.scss';
 
 function App() {
+  const dispatch = useAppDispatch();
+  const [skip, setSkip] = useState(true);
+  const citiesOffset = Number(localStorage.getItem('citiesOffset'));
   const { offset, limit, countryIds } = useAppSelector(
     (state) => state.citiesPaginationReducer,
   );
 
-  const { data, isLoading, error } = useGetCitiesQuery({
-    offset,
-    limit,
-    countryIds,
-  });
+  const { data, isLoading, error } = useGetCitiesQuery(
+    {
+      offset,
+      limit,
+      countryIds,
+    },
+    { skip },
+  );
+
+  useEffect(() => {
+    if (citiesOffset) {
+      dispatch(setOffset(citiesOffset));
+      setSkip(false);
+    } else {
+      setSkip(false);
+    }
+  }, [citiesOffset]);
 
   return (
     <div className="App">
--- src/components/CitiesList/CitiesList.tsx
+++ src/components/CitiesList/CitiesList.tsx
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import List from '@mui/material/List';
 import { CitiesCard } from '../CitiesCard/CitiesCard';
 import { useGetCitiesQuery } from '../../services/CitiesService';
 import { City } from '../../types/city';
-import { useAppSelector } from '../../hooks/redux';
+import { useAppSelector, useAppDispatch } from '../../hooks/redux';
+import { setOffset } from '../../store/reducers/CitiesPagination';
 
 export const CitiesList = () => {
+  const dispatch = useAppDispatch();
+  const [skip, setSkip] = useState(true);
+  const citiesOffset = Number(localStorage.getItem('citiesOffset'));
   const { offset, limit, countryIds } = useAppSelector(
     (state) => state.citiesPaginationReducer,
   );
 
-  const { data: cities } = useGetCitiesQuery({ offset, limit, countryIds });
+  const { data: cities } = useGetCitiesQuery(
+    { offset, limit, countryIds },
+    { skip },
+  );
+
+  useEffect(() => {
+    if (citiesOffset) {
+      dispatch(setOffset(citiesOffset));
+      setSkip(false);
+    } else {
+      setSkip(false);
+    }
+  }, [citiesOffset]);
 
   return (
     <List className="list list--cities">
